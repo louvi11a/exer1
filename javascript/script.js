35be34cf6f2fd2019644c30ef0314d7e3d22ed1c
Louvilla Limpin <133623871+louvi11a@users.noreply.github.com>
2023-09-08 10:04:19 +0800

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,13 @@
-    <script>
-        function displaySum() {
-            let firstNum = Number(document.getElementById('firstNum').innerHTML);
-            let secondNum = Number(document.getElementById('secondNum').innerHTML);
+function displaySum() {
+    let firstNum = Number(document.getElementById('firstNum').innerHTML);
+    let secondNum = Number(document.getElementById('secondNum').innerHTML);
 
-            let total = firstNum + secondNum;
-            document.getElementById("answer").innerHTML = `${firstNum} + ${secondNum}, equals to ${total}`;
-        }
+    let total = firstNum + secondNum;
+    let answerElement = document.getElementById("answer");
+    
+    answerElement.textContent = `${firstNum} + ${secondNum}, equals to ${total}`;
+    answerElement.style.fontSize = "24px";
+    answerElement.style.fontWeight = "bold";
+}
 
-        document.getElementById('sumButton').addEventListener("click", displaySum);
-    </script>
+document.getElementById('sumButton').addEventListener("click", displaySum);
